57fe6127db5047c06da8c44e10477bdc93bdaf83
Eli <100180307+EliTheGingerCat@users.noreply.github.com>
2024-05-05 21:21:37 -0400

    Web sockets work, going to fix Content-Type header so JavaScript modules can load and then work on an actual multiplayer JavaScript game.

--- src/static/js/web-socket.js
+++
@@ -1,9 +0,0 @@
-const socket = new WebSocket("ws://localhost:8080")
-
-socket.addEventListener("open", event => {
-	socket.send("hi")
-})
-
-socket.addEventListener("message", event => {
-	console.log(event.data)
-})
